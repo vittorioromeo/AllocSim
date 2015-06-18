47ace9264f4bff4eb5866c294b7899ef074874c7
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-06-18 16:18:40 +0200

    manual update

--- docs/latex/mkThesis.sh
+++ docs/latex/mkThesis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 latexpp ./manual.lpp > ./manual.tex
-pdflatex -shell-escape ./manual.tex && pdflatex -shell-escape ./manual.tex && chromium ./manual.pdf
+pdflatex -shell-escape ./manual.tex && pdflatex -shell-escape ./manual.tex && chromium ./manual.pdf && cp ./manual.pdf ../manual.pdf
